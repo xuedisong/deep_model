070c82d2759a76947013ecf61787995e63e29c95
lixin15 <lixin15@yiche.com>
2022-04-06 10:17:06 +0800

    feature code review

--- src/bean/feature.py
+++ src/bean/feature.py
@@ -1,14 +1,9 @@
-from typing import Mapping
-from typing import Tuple
+from typing import Mapping, List
 
-from common.constant import *
 from utils import json_util
-from utils.args_util import FLAGS
-from utils.stream_util import *
+
+
 # 特征类
-from utils.stream_util import Stream
-
-
 class Feature(object):
 
     def __init__(self):
@@ -54,58 +49,3 @@ class FeatureBO(object):
         """
         self.featureMap: Mapping[str, Feature] = _featureMap
         self.featureValuesMap: Mapping[str, List[str]] = _featureValuesMap
-
-
-def featureList2featureBO(_featureList: List[Feature]) -> FeatureBO:
-    """
-    featureList to FeatureBO
-    :param _featureList: featureList
-    :return: FeatureBO
-    """
-    pass
-
-
-def parse_feature(_feature_info_path: str, _feature_dict_path: str) -> List[Feature]:
-    """
-    解析特征
-    :param _feature_info_path: 特征文件
-    :param _feature_dict_path: 特征值文件
-    :return: 特征列表
-    """
-    with open(_feature_info_path) as stream:
-        stream: Iterable[str] = stream
-        skip_features: List[str] = Stream(stream).find_first().strip().split(STR_SKIP_FEATURES)[1].split(
-            DELIMITER_COMMA)
-        print("APP skip_features:", skip_features)
-
-        def line_to_feature(line: str) -> Feature:
-            line_list: List[str] = line.strip().split(DELIMITER_TAB)
-            feature = Feature()
-            feature.set_index(int(line_list[0]))
-            feature.set_name(line_list[1])
-            feature.set_type(line_list[2])
-            return feature
-
-        featureList: List[Feature] = Stream(stream).map(line_to_feature).to_list()
-    with open(_feature_dict_path) as stream:
-        stream: Iterable[str] = stream
-
-        def line_to_nameValue(line: str) -> Tuple[str, str]:
-            lineList = line.strip().split(DELIMITER_TAB)
-            nameValueList = lineList[0].split('-')[1].split('^')
-            name: str = nameValueList[0]
-            value: str = nameValueList[1]
-            return name, value
-
-        nameValueMap: Dict[str, List[Tuple[str, str]]] = Stream(stream).map(line_to_nameValue).group_by(
-            lambda nameValue: nameValue[0])
-        nameValueMap = {k: Stream(v).map(lambda x: x[1]).distinct().to_list() for k, v in nameValueMap.items()}
-    Stream(featureList).for_each(lambda feature: feature.set_valueList(nameValueMap.get(feature.get_name())))
-    return featureList
-
-
-if __name__ == '__main__':
-    _feature_info_path = FLAGS.feature_info
-    _feature_dict_path = FLAGS.dict_path
-    feature_list = parse_feature(_feature_info_path, _feature_dict_path)
-    Stream(feature_list).print()
--- src/biz/feature_biz.py
+++ src/biz/feature_biz.py
@@ -1,49 +1,62 @@
-# from typing import List, Mapping
-#
-# from bean.feature import FeatureBO, Feature
-# from utils.args_util import FLAGS
-# from utils.stream_util import Stream
-#
-# feature_info_path = FLAGS.feature_info
-# feature_dict_path = FLAGS.dict_path
-#
-#
-# def buildFeatureInfo(_feature_info_path, _feature_dict_path) -> FeatureInfo:
-#     """
-#     创建特征信息
-#     :param _feature_info_path: 特征文件
-#     :param _feature_dict_path: 特征值文件
-#     :return: 特征信息
-#     """
-#     pass
-#
-#
-# def parseFeatureList(_feature_info_path: str) -> List[Feature]:
-#     """
-#     解析特征列表
-#     :param _feature_info_path: 特征文件
-#     :return: 特征列表
-#     """
-#     result = dict()
-#
-#
-#     print('Finish feature info parse, feature num is {}'.format(len(self.feature_list)))
-#     print(self.feature_list)
-#     return result
-#
-#
-# def parseFeatureValuesMap(_feature_dict_path: str) -> Mapping[str, List[str]]:
-#     """
-#     解析特征取值列表
-#     :param _feature_dict_path: 特征值文件
-#     :return: 特征值列表
-#     """
-#     vocabulary_map = dict()
-#     with open(dict_info_file) as stream:
-#         for line in stream:
-#             data = line.strip().split('\t')
-#             feature_name = data[0].split('-')[1].split('^')[0]
-#             vocabulary_map.setdefault(feature_name, []).append(data[0])
-#     print('Finish dict info parse, vocabulary feature number is {}'.format(len(vocabulary_map)))
-#     print(vocabulary_map.keys())
-#     return vocabulary_map
+from typing import Tuple
+
+from bean.feature import Feature, FeatureBO
+from common.constant import *
+from utils.args_util import FLAGS
+from utils.stream_util import *
+from utils.stream_util import Stream
+
+
+def featureList2featureBO(_featureList: List[Feature]) -> FeatureBO:
+    """
+    featureList to FeatureBO
+    :param _featureList: featureList
+    :return: FeatureBO
+    """
+    pass
+
+
+def parse_feature(_feature_info_path: str, _feature_dict_path: str) -> List[Feature]:
+    """
+    解析特征
+    :param _feature_info_path: 特征文件
+    :param _feature_dict_path: 特征值文件
+    :return: 特征列表
+    """
+    with open(_feature_info_path) as stream:
+        stream: Iterable[str] = stream
+        skip_features: List[str] = Stream(stream).find_first().strip().split(STR_SKIP_FEATURES)[1].split(
+            DELIMITER_COMMA)
+        print("APP skip_features:", skip_features)
+
+        def line_to_feature(line: str) -> Feature:
+            line_list: List[str] = line.strip().split(DELIMITER_TAB)
+            feature = Feature()
+            feature.set_index(int(line_list[0]))
+            feature.set_name(line_list[1])
+            feature.set_type(line_list[2])
+            return feature
+
+        featureList: List[Feature] = Stream(stream).map(line_to_feature).to_list()
+    with open(_feature_dict_path) as stream:
+        stream: Iterable[str] = stream
+
+        def line_to_nameValue(line: str) -> Tuple[str, str]:
+            lineList = line.strip().split(DELIMITER_TAB)
+            nameValueList = lineList[0].split('-')[1].split('^')
+            name: str = nameValueList[0]
+            value: str = nameValueList[1]
+            return name, value
+
+        nameValueMap: Dict[str, List[Tuple[str, str]]] = Stream(stream).map(line_to_nameValue).group_by(
+            lambda nameValue: nameValue[0])
+        nameValueMap = {k: Stream(v).map(lambda x: x[1]).distinct().to_list() for k, v in nameValueMap.items()}
+    Stream(featureList).for_each(lambda feature: feature.set_valueList(nameValueMap.get(feature.get_name())))
+    return featureList
+
+
+if __name__ == '__main__':
+    _feature_info_path = FLAGS.feature_info
+    _feature_dict_path = FLAGS.dict_path
+    feature_list = parse_feature(_feature_info_path, _feature_dict_path)
+    Stream(feature_list).print()
