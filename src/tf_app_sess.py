a8c35258135c06761060e938be11f9deffd6344d
lixin15 <lixin15@yiche.com>
2023-03-05 12:06:58 +0800

    add tf 记录中间过程至tfb;

--- src/tf_app_sess.py
+++ src/tf_app_sess.py
@@ -1,45 +1,34 @@
 import tensorflow as tf
-
 import numpy as np
 
-# 随机生成 100 点（x，y）
-# [0,1）上的均匀分布
+# 随机生成 100 点（x，y）, [0,1）上的均匀分布
 x_data = np.random.rand(10).astype(np.float32)
-
 y_data = x_data * 0.1 + 0.3
 
-# 构建线性模型的 tensor 变量 W, b
-
 W = tf.Variable(tf.random_uniform([1], -1.0, 1.0))
-
 b = tf.Variable(tf.zeros([1]))
-
 y = W * x_data + b
-
-# 构建损失方程，优化器及训练模型操作 train
-
 loss = tf.reduce_mean(tf.square(y - y_data))
 
-optimizer = tf.train.GradientDescentOptimizer(0.001) # 0.5
+tf.summary.scalar("loss", loss)
 
+optimizer = tf.train.GradientDescentOptimizer(0.5)
 train = optimizer.minimize(loss)
 
-# 构建变量初始化操作 init
-
-init = tf.initialize_all_variables()
-
-# 构建 TensorFlow session
-
 sess = tf.Session()
-
-# 初始化所有 TensorFlow 变量
-
-sess.run(init)
-
-# 训练该线性模型，每隔 20 次迭代，输出模型参数
+sess.run(tf.global_variables_initializer)
+saver = tf.train.Saver(tf.global_variables(), max_to_keep=2500)
+merged = tf.summary.merge_all()
+write = tf.summary.FileWriter('/Users/yiche/dev/code/deep_model/model/esmm_block', sess.graph)
 
 for step in range(201):
-    if step % 20 == 0:
-        print(step, sess.run(W), sess.run(b), x_data, sess.run(y))
+    if step % 1 == 0:
+        print(step, sess.run(W), sess.run(b), sess.run(loss))  # x_data, sess.run(y)
+        result = sess.run(merged)
+        write.add_summary(result, step)
+    if step % 5 == 0:
+        saver.save(sess, '/Users/yiche/dev/code/deep_model/model/esmm_block' + '/model.ckpt', global_step=step,
+                   write_meta_graph=True)
     sess.run(train)
+write.close()
 sess.close()
