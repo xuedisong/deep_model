0225aa0cbca4b44efdbf03a60c9ee892afa9db51
lixin15 <lixin15@yiche.com>
2023-03-06 14:56:13 +0800

    add auc metric custom

--- src/tf_app_sess.py
+++ src/tf_app_sess.py
@@ -1,6 +1,30 @@
 import tensorflow as tf
 import numpy as np
-from sklearn.metrics import roc_auc_score
+
+
+# from sklearn.metrics import roc_auc_score
+
+def roc_auc_score(labels, preds, n_bins=100):
+    postive_len = sum(labels)
+    negative_len = len(labels) - postive_len
+    total_case = postive_len * negative_len
+    pos_histogram = [0 for _ in range(n_bins)]
+    neg_histogram = [0 for _ in range(n_bins)]
+    bin_width = 1.0 / n_bins
+    for i in range(len(labels)):
+        nth_bin = int(preds[i] / bin_width)
+        if labels[i] == 1:
+            pos_histogram[nth_bin] += 1
+        else:
+            neg_histogram[nth_bin] += 1
+    accumulated_neg = 0
+    satisfied_pair = 0
+    for i in range(n_bins):
+        satisfied_pair += (pos_histogram[i] * accumulated_neg + pos_histogram[i] * neg_histogram[i] * 0.5)
+        accumulated_neg += neg_histogram[i]
+
+    return satisfied_pair / float(total_case)
+
 
 y_true = [1, 1, 0, 0, 1, 1, 0]
 y_pred = [0.8, 0.7, 0.5, 0.5, 0.5, 0.5, 0.3]
